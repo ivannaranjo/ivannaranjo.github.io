27b406fa055527635bd1e2852a5c4185855828e9
Ivan Naranjo <ivan.naranjo@gmail.com>
2018-09-23 15:27:43 +0100

    Run the site.

---
+++ tools/run_site.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+# Runs the site within a docker image.
+
+docker run \
+    -it \
+    -p 4000:4000 \
+    -v ${HOME}/github/ivannaranjo.github.io/:/var/repo/:rw \
+    --rm \
+    --workdir /var/repo/ \
+    jekyll \
+    bundle exec jekyll serve --host 0.0.0.0
